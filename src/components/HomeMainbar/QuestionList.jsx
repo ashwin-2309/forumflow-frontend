0da7c9a8b8281df6a27f9a9bdf154da2cd5924b1
Ashwin Kumar Singh <ashwinkumar2309@protonmail.com>
2023-08-21 10:08:31 +0530

    fixed the questionList'

--- src/components/HomeMainbar/QuestionList.jsx
+++ src/components/HomeMainbar/QuestionList.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import Questions from "./Questions";
+
 const QuestionList = ({ questionsList }) => {
+  console.log(questionsList);
+  if (!questionsList) {
+    return <></>; // Return empty if questionsList is not available yet
+  }
+
   return (
     <>
       {questionsList.map((question) => (
